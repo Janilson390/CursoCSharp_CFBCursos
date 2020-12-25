5f6d165345bceccd58d5ee615c3999642a91c4fb
Janilson Florencio <janilson390@gmail.com>
2020-12-25 14:10:38 -0300

    Aula 06 - Formatando a Saida no Console 	- .gitignore: excluir arquivos *.exe 	- Conteudo da Aula 06

---
+++ Aula06.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Aula06{
+    static void Main(){
+        double PrecoUnitario, Lucro, ValorVenda;
+        string NomeProduto;
+
+        Console.Write("Digite o nome do produto ");
+        NomeProduto = Console.ReadLine();
+
+        Console.Write("Digite valor de compra ");
+        PrecoUnitario = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a margem de lucro ");
+        Lucro = Convert.ToDouble(Console.ReadLine());
+
+        ValorVenda = PrecoUnitario + (PrecoUnitario * Lucro);
+        
+        Console.WriteLine("Produto...................{0,10}", NomeProduto);
+        Console.WriteLine("Valor de Compra...........{0,10:c}", PrecoUnitario);
+        Console.WriteLine("Margem de Lucro...........{0,10:p}", Lucro);
+        Console.WriteLine("Valor de Venda............{0,10:c}", ValorVenda);
+        Console.ReadKey();
+    }
+}
