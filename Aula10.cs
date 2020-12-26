6bdca27bc6a8b830577d05537204a04359c29780
Janilson Florencio <janilson390@gmail.com>
2020-12-26 10:22:23 -0300

    Aula 10 - Enumeradores Acessando os valores do enumerador pela referência do Índice.

--- Aula10.cs
+++ Aula10.cs
@@ -4,7 +4,7 @@ class Aula10{
     enum DiasSemanda{Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado};
     static void Main()
     {
-        DiasSemanda ds = DiasSemanda.Domingo;
+        DiasSemanda ds = (DiasSemanda)3;
 
         Console.WriteLine("O dia da semana é {0}", ds);
 
