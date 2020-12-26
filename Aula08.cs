5ef65616c54d1da219d6be1ca7982af57de728ff
Janilson Florencio <janilson390@gmail.com>
2020-12-25 22:17:31 -0300

    Aula 08 - Operadores BitWise - Utilizando o operador >> (corta pela metade); - Correção do exemplo anterior.

--- Aula08.cs
+++ Aula08.cs
@@ -6,8 +6,12 @@ class Aula08{
 
         Console.WriteLine("O Valor é de {0}", Valor);
 
-        Valor << 1;
+        Valor = Valor << 1;
 
         Console.WriteLine("Agora o Valor com << é de {0}", Valor);
+
+        Valor = Valor >> 1;
+
+        Console.WriteLine("Agora o Valor com >> é de {0}", Valor);
     }   
 }
