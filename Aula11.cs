56efae8a7ab580477f66cca3265c62eed5368ab8
Janilson Florencio <janilson390@gmail.com>
2020-12-26 10:43:12 -0300

    Aula 11 - Type Cast Conversão implicita de INT para FLOAT.

--- Aula11.cs
+++ Aula11.cs
@@ -2,6 +2,9 @@ using System;
 class Aula10{
     static void Main()
     {
-        
+        int num = 10;
+        float num2 = num;
+
+        Console.WriteLine("Valor de num2 {0}", num);
     }   
 }
