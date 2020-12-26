e76a56a4cb41529c13ded64e22bab4fa083f9cbb
Janilson Florencio <janilson390@gmail.com>
2020-12-26 10:30:51 -0300

    Aula 10 - Enumeradores Atribuindo o indice do enumerador a uma váriavel inteira.

--- Aula10.cs
+++ Aula10.cs
@@ -4,9 +4,8 @@ class Aula10{
     enum DiasSemanda{Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado};
     static void Main()
     {
-        DiasSemanda ds = (DiasSemanda)3;
-
-        Console.WriteLine("O dia da semana é {0}", ds);
-
+        int ds = Convert.ToInt32(DiasSemanda.Sexta);
+        int ds2 = (int)DiasSemanda.Sabado;
+        Console.WriteLine("O dia da semana é {0}\nO dia da semana é {1}", ds, ds2);
     }   
 }
