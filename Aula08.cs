de83b2919151d168c30f1a90476446a7b1547465
Janilson Florencio <janilson390@gmail.com>
2020-12-25 22:14:24 -0300

    Aula 08 - Operadores BitWise Utilizando o operador << (dobra o valor)

--- Aula08.cs
+++ Aula08.cs
@@ -2,6 +2,12 @@ using System;
 
 class Aula08{
     static void Main(){
-        
+        int Valor = 10;
+
+        Console.WriteLine("O Valor é de {0}", Valor);
+
+        Valor << 1;
+
+        Console.WriteLine("Agora o Valor com << é de {0}", Valor);
     }   
 }
