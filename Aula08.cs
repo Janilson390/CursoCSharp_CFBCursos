aa80e08dceb5c282685ac37f0de58e2fe3d2c57d
Janilson Florencio <janilson390@gmail.com>
2020-12-25 22:21:17 -0300

    Aula 08 - Operadores BitWise Utlizando >> e << com 2 (dobre em 2 e corta pela metade em 2).

--- Aula08.cs
+++ Aula08.cs
@@ -10,8 +10,18 @@ class Aula08{
 
         Console.WriteLine("Agora o Valor com << é de {0}", Valor);
 
+        Valor = 10;
         Valor = Valor >> 1;
 
         Console.WriteLine("Agora o Valor com >> é de {0}", Valor);
+
+        Valor = 10;
+        Valor = Valor << 2;
+
+        Console.WriteLine("Agora o Valor com << 2 é de {0}", Valor);
+
+        Valor = Valor >> 2;
+
+        Console.WriteLine("Agora o Valor com >> 2 é de {0}", Valor);
     }   
 }
