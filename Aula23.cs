9d91d9e4b7a66499bef68eac9f4707a580bc3f13
Janilson Florencio <janilson390@gmail.com>
2021-01-13 11:15:17 -0300

    Aula 23 - Arrays, Matrizes e Acessos Acesso a um vetor por uma estrutura de loop(FOR e FOREACH).

--- Aula23.cs
+++ Aula23.cs
@@ -2,11 +2,20 @@ using System;
 class Aula23{
     static void Main()
     {
-      int[] vetor1= new int[5];
-      int[] vetor2= new int[5];
-      int[] vetor3= new int[5];
-      int [,] matriz = new int[2,5]{{11, 22, 00m 44, 55}, {66, 77, 88, 99, 00}};
+        int[] vetor1= new int[5];
+        int[] vetor2= new int[5];
+        int[] vetor3= new int[5];
+        int[,] matriz = new int[2,5]{{11, 22, 00, 44, 55}, {66, 77, 88, 99, 00}};
 
-      Random random = new Random();
+        Random random = new Random();
+        
+        for(int i=0; i < vetor1.Length; i++){
+            vetor1[i]=random.Next(50);
+        }
+
+        Console.WriteLine("Elementos do vetor1 ");
+        foreach (int v in vetor1){
+            Console.Write("{0} ", v);
+        }
     }   
 }
