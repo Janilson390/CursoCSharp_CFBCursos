a21f7480595947dff3c7b394445800c63f3f2e1c
Janilson Florencio <janilson390@gmail.com>
2021-01-13 11:05:12 -0300

    Aula 23 - Arrays, Matrizes e Acessos Declaração das variaveis.

--- Aula23.cs
+++ Aula23.cs
@@ -2,6 +2,11 @@ using System;
 class Aula23{
     static void Main()
     {
-      
+      int[] vetor1= new int[5];
+      int[] vetor2= new int[5];
+      int[] vetor3= new int[5];
+      int [,] matriz = new int[2,5]{{11, 22, 00m 44, 55}, {66, 77, 88, 99, 00}};
+
+      Random random = new Random();
     }   
 }
