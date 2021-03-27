ff30c77dba1b1494827196e3c8cabdc5f2d1ed54
Janilson Florencio <janilson390@gmail.com>
2021-03-27 16:27:10 -0300

    Aula 31 Classes Static - Commit Inicial

---
+++ Aula31.cs
@@ -0,0 +1,47 @@
+using System;
+
+public class Jogador{
+    public int energia;
+    public bool vivo;
+    public string nome;
+
+    public Jogador(){
+        energia = 100;
+        vivo = true;
+    }
+    
+    public Jogador(string n){
+        energia = 100;
+        vivo = true;
+        nome = n;
+    }
+
+    public Jogador(string n, int e, bool v){
+        energia = e;
+        vivo = v;
+        nome = n;
+    }
+
+    public void info(){
+        Console.WriteLine("Nome jogador.......{0}", nome);
+        Console.WriteLine("Energia jogador....{0}", energia);
+        Console.WriteLine("Estado jogador.....{0}\n", vivo?"Vivo":"Morto");
+    }
+
+    //~Jogador(){
+    //    Console.WriteLine("Jogador {0} Destruido!", nome);
+    //}
+}
+class Aula31{
+    static void Main(){
+        Jogador j1 = new Jogador();
+        Jogador j2 = new Jogador("Bruno");
+        Jogador j3 = new Jogador("Théo", 50, true);
+        Jogador j4 = new Jogador("Benegundesfirno", 0, false);
+
+        j1.info();
+        j2.info();
+        j3.info();
+        j4.info();        
+    }
+}
