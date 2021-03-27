b6ef1d87ddae550d7ebfde20976a9652270c178e
Janilson Florencio <janilson390@gmail.com>
2021-03-27 16:13:51 -0300

    Aula 30 Sobrecarga de Construtores Exemplos da Aula

--- Aula30.cs
+++ Aula30.cs
@@ -9,29 +9,39 @@ public class Jogador{
         energia = 100;
         vivo = true;
     }
+    
     public Jogador(string n){
         energia = 100;
         vivo = true;
         nome = n;
     }
 
-    ~Jogador(){
-        Console.WriteLine("Jogador {0} Destruido!", nome);
+    public Jogador(string n, int e, bool v){
+        energia = e;
+        vivo = v;
+        nome = n;
     }
+
+    public void info(){
+        Console.WriteLine("Nome jogador.......{0}", nome);
+        Console.WriteLine("Energia jogador....{0}", energia);
+        Console.WriteLine("Estado jogador.....{0}\n", vivo?"Vivo":"Morto");
+    }
+
+    //~Jogador(){
+    //    Console.WriteLine("Jogador {0} Destruido!", nome);
+    //}
 }
 class Aula29{
     static void Main(){
-        Jogador j1 = new Jogador("Bruno");
-        Jogador j2 = new Jogador();
-        Jogador j3 = new Jogador("Théo");
+        Jogador j1 = new Jogador();
+        Jogador j2 = new Jogador("Bruno");
+        Jogador j3 = new Jogador("Théo", 50, true);
+        Jogador j4 = new Jogador("Benegundesfirno", 0, false);
 
-        j1.energia =50;
-        Console.WriteLine("Nome do Jogador 1: {0}", j1.nome);
-        Console.WriteLine("Nome do Jogador 2: {0}", j2.nome);
-        Console.WriteLine("Nome do Jogador 3: {0}", j3.nome);
-
-        j2.nome = Console.ReadLine().ToString();
-
-        Console.WriteLine("\nNome do Jogador 2: {0}", j2.nome);
+        j1.info();
+        j2.info();
+        j3.info();
+        j4.info();        
     }
 }
