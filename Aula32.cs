ca811ede3fa48109da4125a99b26d228a2e19216
Janilson Florencio <janilson390@gmail.com>
2021-03-27 17:36:43 -0300

    Aula 32 Classes Static - Commit Inicial

---
+++ Aula32.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Calculos{
+    public int v1;
+    public int v2;
+
+    public Calculos(int v1, int v2){
+        v1 = v1;
+        v2 = v2;
+    }
+
+    public void Soma(){
+        Console.WriteLine("Soma é {0}", v1+v2);
+    }
+}
+class Aula31{
+    static void Main(){
+      
+    }
+}
