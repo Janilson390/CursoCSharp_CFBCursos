04cfdb578f4343913cf92ca34ad6a965dad13585
Janilson Florencio <janilson390@gmail.com>
2021-03-27 17:46:45 -0300

    Aula 32 Palavra Chave This - Primeiro Exemplo Aula

--- Aula32.cs
+++ Aula32.cs
@@ -5,16 +5,26 @@ public class Calculos{
     public int v2;
 
     public Calculos(int v1, int v2){
-        v1 = v1;
-        v2 = v2;
+        this.v1 = v1;
+        this.v2 = v2;
     }
 
     public void Soma(){
-        Console.WriteLine("Soma é {0}", v1+v2);
+        Console.WriteLine("Soma é {0}\n", v1+v2);
+    }
+
+    public int Soma1(){
+        return v1+v2;
     }
 }
-class Aula31{
+class Aula32{
     static void Main(){
-      
+        Calculos c1 = new Calculos(10, 20);
+
+        c1.Soma();
+
+        int res = c1.Soma1();
+        
+        Console.WriteLine("Soma1 é {0}\n", res);
     }
 }
