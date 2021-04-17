c5d907b01f38e093a0142f5eec346e069eeec5a7
Janilson Florencio <janilson390@gmail.com>
2021-04-17 20:57:27 -0300

    Aula 34 - Herança - Acessando as propriedades da classe base por métodos. - Algumas propriedades da classe Base estão privadas

--- Aula34.cs
+++ Aula34.cs
@@ -2,26 +2,32 @@ using System;
 
 class Veiculo{ //Classe Base
     public int velMax;
-    public bool ligado;
+    private bool ligado;
     public void Ligar(){
         ligado = true;
     }   
     public void Desligar(){
         ligado = false;
     }
+
+    public bool getLigado(){
+        return ligado;
+    }
 }
 
 class Carro:Veiculo{
     public Carro(){
-        ligado = true;
+        Desligar();
     }
 }
 class Aula34{
     static void Main(){
         Carro c1 = new Carro();
 
+        Console.WriteLine("O carro está {0}", c1.getLigado()?"Ligado":"Desligado");
+
         c1.Ligar();
 
-        Console.WriteLine("O carro está {0}", c1.ligado?"Ligado":"Desligado");
+        Console.WriteLine("O carro está {0}", c1.getLigado()?"Ligado":"Desligado");
     }
 }
