f2153e13a75155436bbabb98b8ac1a7f32423452
Janilson Florencio <janilson390@gmail.com>
2021-04-17 19:35:27 -0300

    Aula 33 - Public vs Private Primeiro exemplo com public.

--- Aula33.cs
+++ Aula33.cs
@@ -1,6 +1,18 @@
 using System;
 
+class Jogador{
+    public int energia;
+    public string nome;
+    public Jogador(string nome){
+        this.nome = nome;
+        energia=100;
+    }
+}
+
 class Aula33{
     static void Main(){
+        Jogador j1 = new Jogador("Janilson");
+
+        Console.WriteLine("Nome: {0} \nEnergia: {1}", j1.nome, j1.energia);
     }
 }
