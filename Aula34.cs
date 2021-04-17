fe336824e7a9db5af5dfd0f5f274c544b8a81611
Janilson Florencio <janilson390@gmail.com>
2021-04-17 20:49:32 -0300

    Aula 34 - Herança - Exemplo de herança, acessando os métodos da classe base pela derivada.

--- Aula34.cs
+++ Aula34.cs
@@ -1,7 +1,27 @@
 using System;
 
+class Veiculo{ //Classe Base
+    public int velMax;
+    public bool ligado;
+    public void Ligar(){
+        ligado = true;
+    }   
+    public void Desligar(){
+        ligado = false;
+    }
+}
+
+class Carro:Veiculo{
+    public Carro(){
+        ligado = true;
+    }
+}
 class Aula34{
     static void Main(){
-         
+        Carro c1 = new Carro();
+
+        c1.Ligar();
+
+        Console.WriteLine("O carro está {0}", c1.ligado?"Ligado":"Desligado");
     }
 }
